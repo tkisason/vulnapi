70242fe5df9c7f2536696ffa3a3c3e5bd1dd9c5a
Tonimir Kisasondi <kisasondi@gmail.com>
2019-12-30 00:51:53 +0100

    fastapi is fun

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vulnapi
 Intentionaly very vulnerable API with bonus bad coding practices
+
+*NOTE: This is by design a vulnerable application. It's use is to demonstrate bad coding practices and to show off specific vulnerabilities that are not well presented in some other projects, especially some fickle and subtile things.*
+
+*DO NOT USE ANY PIECE OF THIS CODE IN PRODUCTION!*
+
+
+Running:
+
+`uvicorn main:app --reload`
+
---
+++ main.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+from datetime import datetime, timedelta
+
+import json
+import jwt
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+SECRET_KEY = "123456"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 600
+
+
+accounts_db = json.loads( open('data/accounts.json').read())
+users_db = json.loads( open('data/users.json').read())
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str = None
+
+class User(BaseModel):
+    username: str
+    email: str = None
+    full_name: str = None
+    disabled: bool = None
+
+class UserInDB(User):
+    hashed_password: str
+
+class Accounts(BaseModel):
+    account: str
+    username: str
+    amount: float
+
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    for user in db:
+        if username == user['username']:
+            return UserInDB(**user)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except:
+        raise credentials_exception
+    user = get_user(users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+@app.get("/accounts")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    customer = [customer for customer in accounts_db['data'] if customer['username'].find(current_user.username)>=0]
+    return customer[0]
+
+
+'''
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+'''
+
+
+
+
+# /accounts/{accountid}?dateFrom=<date>&dateTo=<date>&entryReferenceFrom=<string>&bookingStatus=<string>&deltaList=<boolean>&withBalance=<boolean>
+
+
+
+#yolo versioning
+
